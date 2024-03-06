644b10636c5f263e836ccded2160a5584ecd673b
Chen jiangrun <jiangrun.23@intl.zju.edu.cn>
2024-03-06 16:14:04 +0800

    recursive.py

---
+++ Practical4/recursive.py
@@ -0,0 +1,7 @@
+a=4 # the first variable
+for i in range(5): #repeat five times
+    print(a) # display a
+    a=a*2+3  # create a recursive sequence
+
+# plan: cycle five times, and at every times, it will print a and let a double and then add three.
+    
